997aa3ba85eb8ae29c73038eaa687b4c5cec3c48
NyeongB <kdtoaj14@ajou.ac.kr>
2022-04-07 20:12:46 +0900

    내부 컴포넌트

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from "react";
 import Potato from "./potato";
 
+function Tomato() {
+  return <h3>I like tomato</h3>;
+}
+
 function App() {
   return (
     <div>
       <h1>Hello</h1>
       <Potato></Potato>
+      <Tomato></Tomato>
     </div>
   );
 }
