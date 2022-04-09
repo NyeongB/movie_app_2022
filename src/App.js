7cffd8a44c6e56d5fa87716bff2e0386d675259f
NyeongB <kdtoaj14@ajou.ac.kr>
2022-04-09 20:30:36 +0900

    p.92

--- src/App.js
+++ src/App.js
@@ -1,20 +1,48 @@
 import React from "react";
 import Potato from "./potato";
+import "./App.css";
 
-function Food({ fav }) {
-  return <h1>I like {fav}</h1>;
+function Food({ name, picture }) {
+  return (
+    <div className="compo">
+      <h2>I like {name}</h2>
+      <img src={picture}></img>
+    </div>
+  );
 }
 
 function App() {
-  const food1 = "kimchi";
+  const foodLike = [
+    {
+      name: "Kimchi",
+      image:
+        "https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcRtv5s7-bZ8s5Q_yPm8zRFXm1N5u8hfiC2rgZU57zDZkkfyU3PgltzszMIA84EeQnpBznZErHkNy2aDK8KJZnemMPpEtBWFdqqO7oXSJzHZ&usqp=CAc",
+    },
+    {
+      name: "Samgyeopsal",
+      image: "https://dpj8x7518los8.cloudfront.net/2019/06/0-1-1.jpg",
+    },
+    {
+      name: "Bibimbap",
+      image:
+        "https://www.kculture.or.kr/jnrepo/upload/jnBrdBoard/201912/2a64936a26d647eca1c7269fb6b3cf99_1575373232721.jpg",
+    },
+    {
+      name: "Doncasu",
+      image:
+        "https://rimage.gnst.jp/livejapan.com/public/article/detail/a/00/02/a0002274/img/basic/a0002274_main.jpg?20200626102550&q=80&rw=750&rh=536",
+    },
+    {
+      name: "Kimbap",
+      image:
+        "https://recipe1.ezmember.co.kr/cache/recipe/2017/07/27/aa6a94c863535af15b6a7512af579d6b1.jpg",
+    },
+  ];
   return (
     <div>
-      <h1>Hello</h1>
-      <Potato></Potato>
-      <Food fav={food1} something={true} papa={["hello", 1, 2, 3, 4]}></Food>
-      <Food fav="ramen" />
-      <Food fav="samgiopsal" />
-      <Food fav="chukumi" />
+      {foodLike.map((dish) => (
+        <Food name={dish.name} picture={dish.image}></Food>
+      ))}
     </div>
   );
 }
