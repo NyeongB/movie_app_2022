7cc9609e4d880a97afe691a11c39f19e73d15293
NyeongB <kdtoaj14@ajou.ac.kr>
2022-04-09 20:00:30 +0900

    p.78

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React from "react";
 import Potato from "./potato";
 
-function Tomato() {
-  return <h3>I like tomato</h3>;
+function Food({ fav }) {
+  return <h1>I like {fav}</h1>;
 }
 
 function App() {
+  const food1 = "kimchi";
   return (
     <div>
       <h1>Hello</h1>
       <Potato></Potato>
-      <Tomato></Tomato>
+      <Food fav={food1} something={true} papa={["hello", 1, 2, 3, 4]}></Food>
+      <Food fav="ramen" />
+      <Food fav="samgiopsal" />
+      <Food fav="chukumi" />
     </div>
   );
 }
