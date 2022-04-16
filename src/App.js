9b596da0e4d23cedfa17f9b7c8e2d178a9d2b0c0
NyeongB <kdtoaj14@ajou.ac.kr>
2022-04-16 21:20:06 +0900

    p.176( add Movie Component & movie API)

--- src/App.js
+++ src/App.js
@@ -1,21 +1,50 @@
 import React from "react";
 import axios from "axios";
+import Movie from "./Movie";
 
 class App extends React.Component {
   state = {
     isLoading: true,
-    movie: [],
+    movies: [],
+  };
+
+  getMovies = async () => {
+    const {
+      data: {
+        data: { movies },
+      },
+    } = await axios.get(
+      "https://yts-proxy.now.sh/list_movies.json?sort_by=rating"
+    );
+    //console.log(movies);
+    this.setState({ movies, isLoading: false });
   };
 
   componentDidMount() {
     // 영화 데이터 로딩
-
-    axios.get("https://yts-proxy.now.sh/list_movies.json");
+    this.getMovies();
   }
 
   render() {
-    const { isLoading } = this.state;
-    return <>{isLoading ? "Lodaing..." : "We are ready"}</>;
+    const { isLoading, movies } = this.state;
+    return (
+      <>
+        {isLoading
+          ? "Lodaing..."
+          : movies.map((movie) => {
+              console.log(movie);
+              return (
+                <Movie
+                  id={movie.id}
+                  year={movie.year}
+                  title={movie.title}
+                  summary={movie.summary}
+                  poster={movie.poster}
+                ></Movie>
+              );
+            })}
+      </>
+    );
   }
 }
 
