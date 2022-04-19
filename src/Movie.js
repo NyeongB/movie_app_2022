81244737251bd4ab1ec8d137bd4e979735c6eb4b
NyeongB <kdtoaj14@ajou.ac.kr>
2022-04-19 20:50:45 +0900

    p.186

--- src/Movie.js
+++ src/Movie.js
@@ -1,7 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-function Movie({ id, title, year, summary, poster }) {
+function Movie({ title, year, summary, poster }) {
   return (
     <div class="movie__data">
       <img src={poster} alt={title} title={title}></img>
@@ -13,7 +13,6 @@ function Movie({ id, title, year, summary, poster }) {
 }
 
 Movie.prototype = {
-  id: PropTypes.number.isRequired,
   year: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
   summary: PropTypes.string.isRequired,
